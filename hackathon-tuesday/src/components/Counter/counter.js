f2b24338bbdde97027281f550d31f4aec4a27867
NovaGlow <dilyncorbett@gmail.com>
2022-01-11 15:40:22 +0000

    emoji picker added

--- hackathon-tuesday/src/components/Counter/counter.js
+++ hackathon-tuesday/src/components/Counter/counter.js
@@ -13,7 +13,7 @@ function deincrementCount(){
 return (
     <div className='Button'>
     <button className='incbutton' onClick={incrementCount}>Click me!</button>
-    <p>You've clicked {count} times!"{emojis[Math.floor(Math.random() * 12)]}</p>
+    <p>You've clicked {count} times!"{emojis[Math.floor(Math.random() * 13)]}</p>
     <button className='deincbutton' onClick={deincrementCount}>Unclick me!</button>
     </div>
     )
