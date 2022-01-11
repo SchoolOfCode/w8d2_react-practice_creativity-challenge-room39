2fc7703a284bf45f365c04bc1c5919a2ec568bc4
emilysarina <emy.sarina@gmail.com>
2022-01-11 11:42:12 +0000

    added up and down counter

--- hackathon-tuesday/src/components/Counter/counter.js
+++ hackathon-tuesday/src/components/Counter/counter.js
@@ -2,12 +2,20 @@ import { React, useState} from 'react'
 
 function Counter(){
     const [count, setcount] = useState(0)
-
-
-
+    function incrementCount(){
+        setcount (count +1 )
+    }
+function deincrementCount(){
+    setcount (count -1 )
+}    
 return (
-    <div></div>
+    <div>
+    <button onClick={incrementCount}>Click me!</button>
+    <p>You've clicked {count} times!</p>
+    <button onClick={deincrementCount}>Unclick me!</button>
+    </div>
     )
 
 }
 export default Counter 
+
